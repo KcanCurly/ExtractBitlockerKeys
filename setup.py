97c1031b71f573bca71204a9beeccffc1ec1b402
Kerim Can Karaduman <96576479+KcanCurly@users.noreply.github.com>
2024-11-15 12:27:13 +0300

    setup.py 2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     python_requires=">=3.8",
     entry_points={
         "console_scripts": [
-            "ExtractBitlockerKeys=src.python:main",
+            "ExtractBitlockerKeys=python.ExtractBitlockerKeys:main",
         ],
     },
 )
